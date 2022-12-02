85bf1178d5bf26f10742c225a1db1be982e485f0
Nathan Blau <natehhggh@gmail.com>
2022-12-01 19:56:11 -0800

    Solution for Day 1 Implemented

--- Days/1/calorie_counting_rust/src/main.rs
+++ Days/1/calorie_counting_rust/src/main.rs
@@ -1,3 +1,31 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::cmp;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "..Input.txt";
+    println!("path: {}",path);
+    let lines = lines_from_file(path);
+    let mut highest_calories: u32 = 0;
+    let mut curr_calories: u32 = 0;
+    for line in lines {
+        if line != ""{
+            let value:u32 = line.parse().unwrap();
+            curr_calories += value;
+        }else {
+            highest_calories = cmp::max(curr_calories, highest_calories);
+            curr_calories = 0;
+        }
+    }
+
+    println!("Most Calories Held: {}",highest_calories);
+}
+
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines().map(|l| l.expect("Could not parse line"))
+        .collect()
 }
