15dd090ef2402f648988c5f0bba7c70b68b90462
Nathan Blau <natehhggh@gmail.com>
2022-12-02 23:09:04 -0800

    Solution for 2nd star on question 2

--- Days/2/rock_paper_scissors/src/main.rs
+++ Days/2/rock_paper_scissors/src/main.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
 use std::fs::{File};
 use std::io::{BufReader, BufRead};
-use std::u8;
+use std::i8;
 use std::env;
 
-static RESULT_MATRIX: &'static [[u8;3];3] = &[[0,2,1]
+static RESULT_MATRIX: &'static [[i8;3];3] = &[[0,2,1]
                                              ,[1,0,2]
                                              ,[2,1,0]];
 
-static RESULT_SCORES_MATRIX: &'static [[u64;3]] = &[[3,6,0]
+static RESULT_SCORES_MATRIX: &'static [[i64;3]] = &[[3,6,0]
                                                   ,[3,0,6]];
 
 
@@ -19,7 +19,7 @@ fn main() {
     //TODO: why will this not work it's litterally the same as day 1.....
     //let path = "..Input.txt";
     let lines = lines_from_file(path);
-    let player_num:u8 = 1;
+    let player_num:i8 = 1;
     let score = get_player_score(player_num, lines);
 
     println!("Player {} score {}", player_num, score)
@@ -35,41 +35,57 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 }
 
 
-fn get_thrown_idx_p1(thrown: char) -> u32{
-    return (thrown as u32 - 2) % 3;
+fn get_thrown_idx_p1(thrown: char) -> i32{
+    return (thrown as i32 - 2) % 3;
 }
 
 
-fn get_thrown_idx_p2(thrown: char) -> u32{
-    return (thrown as u32 + 2) % 3;
+fn get_thrown_idx_p2(thrown: char) -> i32{
+    return (thrown as i32 + 2) % 3;
 }
 
-fn get_thrown_idx(thrown:Vec<char>) -> [u32; 2]{
-    let thrown_indecies:[u32; 2] = [get_thrown_idx_p1(thrown[0]), get_thrown_idx_p2(thrown[2])];
+fn get_thrown_idx_p2_part_2(thrown:Vec<char>) -> i32{
+    let win_state = (thrown[2] as i32 + 2) % 3 - 1;
+
+    return get_mod((thrown[0] as i32 - 2) + win_state, 3);
+}
+
+fn get_thrown_idx(thrown:Vec<char>) -> [i32; 2]{
+    let thrown_indecies:[i32; 2] = [get_thrown_idx_p1(thrown[0]), get_thrown_idx_p2(thrown[2])];
     return thrown_indecies
 }
 
-fn get_thrown_score(thrown_idx:u32) -> u64{
+fn get_thrown_idx_part_2(thrown:Vec<char>) -> [i32; 2]{
+    let thrown_indecies:[i32; 2] = [get_thrown_idx_p1(thrown[0]), get_thrown_idx_p2_part_2(thrown)];
+    return thrown_indecies
+}
+
+fn get_thrown_score(thrown_idx:i32) -> i64{
     return (thrown_idx + 1).into();
 }
 
-fn get_game_result(thrown:[u32; 2]) -> u8{
+fn get_game_result(thrown:[i32; 2]) -> i8{
     return RESULT_MATRIX[thrown[0] as usize][thrown[1] as usize];
 }
 
-fn get_game_score(result:u8, player_num:u8) -> u64{
-    return RESULT_SCORES_MATRIX[player_num as usize][result as usize]
+fn get_game_score(result:i8, player_num:i8) -> i64{
+    return RESULT_SCORES_MATRIX[player_num as usize][result as usize];
 }
 
 
+fn get_mod(x:i32, m:i32)-> i32{
+    return (x % m + m) % m;
+}
+
 //TODO: refactor into operating on set of p1, and set of p2 moves
-fn get_player_score(player_num:u8, lines:Vec<String>) -> u64{
-    let mut player_score:u64 = 0;
+fn get_player_score(player_num:i8, lines:Vec<String>) -> i64{
+    let mut player_score:i64 = 0;
     for line in lines{
         let strategy_plays:Vec<char> = line.chars().collect();
-        let thrown_indecies = get_thrown_idx(strategy_plays);
-        
-        let result:u8 = get_game_result(thrown_indecies);
+        //let thrown_indecies = get_thrown_idx(strategy_plays);
+        let thrown_indecies = get_thrown_idx_part_2(strategy_plays);
+
+        let result:i8 = get_game_result(thrown_indecies);
         
 
         player_score += get_thrown_score(thrown_indecies[player_num as usize]);
